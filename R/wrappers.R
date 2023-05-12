eee435e3a71c302f06ada10fd9254ddc9029fbe5
Woo <wooh@lmis0744.mskcc.org>
2023-05-12 10:48:29 -0400

    fix

--- R/wrappers.R
+++ R/wrappers.R
@@ -28,42 +28,42 @@ run_tSig <- function(catalog, signat = 'v2', nperm = 1000, alpha = 0.05, output 
 }
 
 #' Run multiple decomposition with bootstrapping
-#' @export
-runBootstrap <- function(catalog, method = 'nmf', verbose = 2, nboot = 100, K = 2, nrun = 100, 
-                          progress.bar = TRUE, Kmin = 2, Kmax = 2, reorder = TRUE, ...){
-  
-  nt <- trinucleotides()
-  nsamp <- NCOL(catalog)
-  M <- colSums(catalog)
-  b <- list()
-  for(i in seq(nboot)){
-    if(verbose >= 2)
-      cat('Bootstrapped run #', i, '...\n', sep='')
-    xcat <- matrix(0, nrow = 96, ncol = nsamp)
-    rownames(xcat) <- nt
-    colnames(xcat) <- colnames(catalog)
-    for(j in seq(nsamp)){
-      tmp <- sample(factor(nt, levels = nt), size = M[j], replace = TRUE, prob = catalog[,j])
-      xcat[,j] <- table(tmp)
-    }
-    b[[i]] <- DeepSig(data = xcat)
-    if(method == 'nmf')
-      b[[i]] <- extractSig(b[[i]], method = method, K = K, nrun = nrun, verbose = verbose - 1, 
-                         progress.bar = progress.bar, ...)
-    else if(method == 'bnmf')
-      b[[i]] <- extractSig(b[[i]], method = method, Kmin = Kmin, Kmax = Kmax, nrun = nrun, verbose = verbose-1,
-                           progress.bar = progress.bar, ...)
-    else stop(paste0('Method ',method,' not allowed'))
-    if(reorder & i > 1){
-      s1 <- signat(b[[1]])
-      si <- signat(b[[i]])
-      cs <- cosineSimilarity(si, s1, diag = FALSE)
-      idx <- clue::solve_LSAP(cs, maximum = TRUE)
-      if(method=='bnmf') K <- Kmax
-      idx <- match(seq(K), idx)
-      b[[i]] <- reorderSig(b[[i]], order = idx)
-    }
-  }
-  
-  return(b)
-}
+##' @export
+#runBootstrap <- function(catalog, method = 'nmf', verbose = 2, nboot = 100, K = 2, nrun = 100, 
+#                         progress.bar = TRUE, Kmin = 2, Kmax = 2, reorder = TRUE, ...){
+#  
+#  nt <- trinucleotides()
+#  nsamp <- NCOL(catalog)
+#  M <- colSums(catalog)
+#  b <- list()
+#  for(i in seq(nboot)){
+#    if(verbose >= 2)
+#      cat('Bootstrapped run #', i, '...\n', sep='')
+#    xcat <- matrix(0, nrow = 96, ncol = nsamp)
+#    rownames(xcat) <- nt
+##    colnames(xcat) <- colnames(catalog)
+#    for(j in seq(nsamp)){
+#      tmp <- sample(factor(nt, levels = nt), size = M[j], replace = TRUE, prob = catalog[,j])
+#      xcat[,j] <- table(tmp)
+#    }
+#    b[[i]] <- DeepSig(data = xcat)
+##    if(method == 'nmf')
+#     b[[i]] <- extractSig(b[[i]], method = method, K = K, nrun = nrun, verbose = verbose - 1, 
+#                         progress.bar = progress.bar, ...)
+#    else if(method == 'bnmf')
+#      b[[i]] <- extractSig(b[[i]], method = method, Kmin = Kmin, Kmax = Kmax, nrun = nrun, verbose = verbose-1,
+#                           progress.bar = progress.bar, ...)
+#    else stop(paste0('Method ',method,' not allowed'))
+##    if(reorder & i > 1){
+#      s1 <- signat(b[[1]])
+#      si <- signat(b[[i]])
+#      cs <- cosineSimilarity(si, s1, diag = FALSE)
+#      idx <- clue::solve_LSAP(cs, maximum = TRUE)
+#      if(method=='bnmf') K <- Kmax
+#      idx <- match(seq(K), idx)
+#      b[[i]] <- reorderSig(b[[i]], order = idx)
+#    }
+#  }
+#  
+#  return(b)
+#}
