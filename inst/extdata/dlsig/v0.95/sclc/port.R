f708f4d286278fa432734e014adfb2bd01840bad
Woo <wooh@mskcc.org>
2024-06-12 11:04:45 -0400

    v0.95 update

--- inst/extdata/dlsig/v0.95/sclc/port.R
+++ inst/extdata/dlsig/v0.95/sclc/port.R
@@ -1,6 +1,6 @@
 ctype <- 'sclc'
 dir <- paste0('/Users/wooh/work/sig/tf/paper/survey/',ctype,'/e3/impact/edist/esample3/theta2/mc2/n1e6/')
-refsig <- paste0('/Users/wooh/work/sig/tf/paper/survey/',ctype,'/',ctype,'_gih_refsig.txt')
+refsig <- paste0('/Users/wooh/work/sig/tf/paper/survey/',ctype,'/',ctype,'_refsig.txt')
 system(paste0('cp ',refsig,' ./refsig.txt'))
 thr.fl <- paste0(dir,'threshold/threshold_cut.txt')
 thr <- read.table(thr.fl,header=TRUE,sep='\t')
